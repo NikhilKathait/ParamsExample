813450c9d05fc6e522ca0c1418764f0888f59111
Nikhil Kathait <nknick97@gmail.com>
2023-01-15 16:22:27 +0530

    Add project files.

---
+++ ParamsExample/Program.cs
@@ -0,0 +1,22 @@
+﻿class Student
+{
+    public void DisplaySubjects(params string[] subjects)
+    {
+        for(int i =0; i < subjects.Length; i++)
+        {
+            System.Console.WriteLine(subjects[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Create object of student class
+        Student s = new Student();
+
+        //Access DisplaySubjects method
+        s.DisplaySubjects("Science", "Mathematics", "Geography", "History", "English");
+    }
+}
